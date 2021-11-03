64482ecee5bd4ddc889dc9fbfcfcd739e558de52
vlaD <vlad.74022@gmail.com>
2021-11-03 23:21:16 +0500

    V1 - ready

--- Generic/List1.cs
+++ Generic/List1.cs
@@ -6,7 +6,92 @@ using System.Threading.Tasks;
 
 namespace Generic
 {
-    class List1
+    public class List1<T>
     {
+        private ListEl<T> last, first;
+
+        public void PushLast(T value)
+        {
+            ListEl<T> item = new();
+            item.Value = value;
+            if (last != null)
+            {
+                item.Previous = last;
+                last.Next = item;
+            }
+            last = item;
+            if (first == null)
+                first = item;
+        }
+
+        public void EnqueueFirst(T value)
+        {
+            ListEl<T> item = new();
+            item.Value = value;
+            if (first != null)
+            {
+                item.Next = first;
+                first.Previous = item;
+            }
+            first = item;
+            if (last == null)
+                last = item;
+        }
+
+        public T PopLast()
+        {
+            T lastValue = this.Last;
+            if (first == last)
+            {
+                first = last = null;
+                return lastValue;
+            }
+            last.Previous.Next = null;
+            last = last.Previous;
+            return lastValue;
+        }
+
+        public T DeqeueFirst()
+        {
+            T firstValue = this.First;
+            if (first == last)
+            {
+                first = last = null;
+                return firstValue;
+            }
+            first.Next.Previous = null;
+            first = first.Next;
+            return firstValue;
+        }
+
+        public T First 
+        { 
+            get
+            {
+                if (!IsEmpty)
+                    return first.Value;
+
+                throw new Exception("Generic type is empty");
+            } 
+        }
+
+        public T Last
+        {
+            get
+            {
+                if (!IsEmpty)
+                    return last.Value;
+
+                throw new Exception("Generic type is empty");
+            }
+        }
+
+        public bool IsEmpty { get { return last == null; } }
+    }
+
+    class ListEl<T>
+    {
+        public ListEl<T> Next, Previous;
+        public T Value;
     }
 }
