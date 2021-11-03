bf69bfd62a33d705a5ce639a809db2d2c423077e
vlaD <vlad.74022@gmail.com>
2021-11-04 00:04:42 +0500

    Push, Pop, Top, IsEmty. What Is Print?

--- Generic/Stack1.cs
+++ Generic/Stack1.cs
@@ -2,7 +2,22 @@
 
 namespace Generic
 {
-    class Stack1
+    class Stack1<T>
     {
+        private Dack<T> dack = new();
+        
+        public void Push(T value)
+        {
+            dack.PushLast(value);
+        }
+
+        public T Pop()
+        {
+            return dack.GetLast();
+        }
+
+        public T Top => dack.Last;
+
+        public bool IsEmpty => dack.IsEmpty;
     }
 }
