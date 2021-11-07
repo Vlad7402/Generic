8d163a4f0c3b912e20dac011ec78187f4be36e32
Савелий <savelussava@mail.ru>
2021-11-07 13:47:33 +0500

    add FileChecker

--- Generic/FileReader.cs
+++ Generic/FileReader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,27 @@ namespace Generic
 {
     public class FileReader
     {
+        public Queue1<object> ReadFile()
+        {
+            var line = File.ReadAllLines("input.txt");
+            if (!CheckFile(line)) throw new Exception("the file is not filled correctly");
+            return ParseLine(line);
+        }
+        
+        private bool CheckFile(string[] line)
+        { return line.Length == 2 && (line[0].ToLower() == "stack" || line[0].ToLower() == "queue"); }
 
+        private Queue1<object> ParseLine(string[] line)
+        {
+            var queue = new Queue1<object>();
+            queue.Enqueue(line[0]);
+            string[] commandLine = line[1].Split(' ');
+            for (int i = 0; i < commandLine.Length; i++)
+            {
+                queue.Enqueue(commandLine[i]);
+            }
+            return queue;
+        }
 
     }
 }
